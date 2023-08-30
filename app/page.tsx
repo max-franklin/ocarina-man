826fd531e76f41e581fc2d7617109992fc6fc63a
Maxwell Franklin <maxwell.e.franklin@gmail.com>
2023-08-30 07:38:22 -0800

    trying out slate styling for page

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,13 @@ import FadeInDiv from '@/components/FadeInDiv'
 export default function Home() {
 
   return (
-    <main className='bg-gradient-to-t from-green-100 px-4'>
+    <main className='bg-slate-100 px-4'>
+    {/* <main className='bg-gradient-to-t from-slate-100 px-4'> */}
       <div className='sm:container sm:mx-auto flex flex-col items-center antialiased text-center text-4xl'>
         <FadeInDiv className='pt-12'>
           <h1 className='pb-4'>Hi, I'm Max!</h1>
           <Image 
-            className='rounded-md'
+            className='rounded-md border border-gray-500'
             src={savageRiverMax}
             alt='Max next to one of the Savage River offshoots in Denali National Park'
             height={480}
@@ -25,7 +26,7 @@ export default function Home() {
         <FadeInDiv className='pt-12'>
           <h1 className='pb-4'>I'm an Alaskan with a love for the outdoors.</h1>
           <Image 
-            className='rounded-md'
+            className='rounded-md border border-gray-500'
             src={mccarthyJumboTrailMax}
             alt='Max on W. ridge at the end of the Jumbo Mine Trail'
             height={480}
@@ -34,7 +35,7 @@ export default function Home() {
         <FadeInDiv className='pt-12'>
           <h1 className='pb-4'>You might have heard me tooting my ocarina if you've ever hiked the chugach!</h1>
           <Image 
-            className='rounded-md'
+            className='rounded-md border border-gray-500'
             src={ocarinaMan}
             alt='Ocarina Man Arctic Valley'
             height={360}
@@ -43,7 +44,7 @@ export default function Home() {
         <FadeInDiv className='pt-12'>
           <h1 className='pb-4'>I'm also a software eningeer with a love for learning new technologies.</h1>
           <Image 
-            className='rounded-md'
+            className='rounded-md border border-gray-500'
             src={airborneDiseaseEstimatoriOS}
             alt="Airhounds' Airborne Disease Estimator iOS App"
             height={480}
