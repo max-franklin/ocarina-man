09423ea4995a0ca7b48322075cf9d5471e6bc9ae
Maxwell Franklin <maxwell.e.franklin@gmail.com>
2023-09-02 09:26:41 -0800

    move component assets into public folder

--- app/_components/FacebookLink.tsx
+++ app/_components/FacebookLink.tsx
@@ -1,6 +1,6 @@
 import IconLink from "./IconLink";
 
-import facebookIcon from '../_assets/4102573_applications_facebook_media_social_icon.png'
+import facebookIcon from '@/public/assets/4102573_applications_facebook_media_social_icon.png'
 
 export default function FacebookLink({ width = 26, height = 26 }) {
     return <IconLink icon={facebookIcon} href={'https://www.facebook.com/Maximillian.Meyer/'} height={height} width={width} />
--- app/_components/LinkedInLink.tsx
+++ app/_components/LinkedInLink.tsx
@@ -1,6 +1,6 @@
 import IconLink from "./IconLink";
 
-import linkedInIcon from '../_assets/5296501_linkedin_network_linkedin logo_icon.png'
+import linkedInIcon from '@/public/assets/5296501_linkedin_network_linkedin logo_icon.png'
 
 export default function LinkedInLink({ width = 26, height = 26 }) {
     return <IconLink icon={linkedInIcon} href={'https://www.linkedin.com/in/mfranklin49'} height={height} width={width} />
